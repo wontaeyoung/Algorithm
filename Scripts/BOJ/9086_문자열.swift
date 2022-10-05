9ca4f18498156f26ccef18950e3c370e95565ae7
Taeyoung Won <hexter1994@gmail.com>
2022-10-05 23:26:29 +0900

    Create 9086_문자열.swift

---
+++ Scripts/BOJ/9086_문자열.swift
@@ -0,0 +1,18 @@
+//
+//  9086_문자열.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/04.
+//
+
+import Foundation
+
+var words = [String]()
+
+for _ in 1...Int(readLine()!)! {
+    words.append(readLine()!)
+}
+
+print(words
+    .map{"\($0.first!)\($0.last!)"}
+    .joined(separator: "\n"))
