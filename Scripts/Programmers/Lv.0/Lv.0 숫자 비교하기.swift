ae8e61825cb661a82d3fed6b25e984e4df02b9d9
Taeyoung Won <hexter1994@gmail.com>
2022-10-10 20:25:05 +0900

    Create Lv.0 숫자 비교하기.swift

---
+++ Scripts/Programmers/Lv.0/Lv.0 숫자 비교하기.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0 숫자 비교하기.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/10.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 == num2 ? 1 : -1
+}
