478119071a0ce167a0f10a0b2b00f51d0f428128
Taeyoung Won <hexter1994@gmail.com>
2022-10-10 21:56:45 +0900

    Update Lv.1 키패드 누르기.swift

--- Scripts/Programmers/Lv.1/Lv.1 키패드 누르기.swift
+++ Scripts/Programmers/Lv.1/Lv.1 키패드 누르기.swift
@@ -73,3 +73,62 @@ func solution(_ numbers:[Int], _ hand:String) -> String
   
   return answer
 }
+
+/*
+ K : 좋은 정답인 것 같아서 참고
+ 
+ import Foundation
+
+ func solution(_ numbers:[Int], _ hand:String) -> String {
+     // 버튼의 위치 테이블
+     let buttonPoses = [1: Position(x: 0, y: 0), 2: Position(x: 1, y: 0), 3: Position(x: 2, y: 0),
+                        4: Position(x: 0, y: 1), 5: Position(x: 1, y: 1), 6: Position(x: 2, y: 1),
+                        7: Position(x: 0, y: 2), 8: Position(x: 1, y: 2), 9: Position(x: 2, y: 2),
+                        0: Position(x: 1, y: 3)]
+     var leftThumbPos = Position(x: 0, y: 3)     // 왼손 엄지는 *에서 시작
+     var rightThumbPos = Position(x: 2, y: 3)    // 오른손 엄지는 #에서 시작
+     var answer = ""
+
+     for number in numbers {
+         // 키를 누르는데 사용할 엄지 손가락 결정
+         let selectedHand: String
+         switch number {
+             case 1, 4, 7:
+                 selectedHand = "L"
+             case 3, 6, 9:
+                 selectedHand = "R"
+             default:
+                 let targetButtonPos = buttonPoses[number]!
+                 let leftThumbDistance = targetButtonPos.manhattanDistance(to: leftThumbPos)
+                 let rightThumbDistance = targetButtonPos.manhattanDistance(to: rightThumbPos)
+                 if leftThumbDistance < rightThumbDistance {
+                     selectedHand = "L"
+                 } else if rightThumbDistance < leftThumbDistance {
+                     selectedHand = "R"
+                 } else {
+                     selectedHand = hand == "left" ? "L" : "R"
+                 }
+         }
+
+         // 버튼을 누른 손가락을 이동시키고 사용한 손가락을 기록
+         let buttonPos = buttonPoses[number]!
+         if selectedHand == "L" {
+             leftThumbPos = buttonPos
+         } else {
+             rightThumbPos = buttonPos
+         }
+         answer += selectedHand
+     }
+
+     return answer
+ }
+
+ struct Position {
+     var x: Int
+     var y: Int
+
+     func manhattanDistance(to: Position) -> Int {
+         return abs(x - to.x) + abs(y - to.y)
+     }
+ }
+ */
