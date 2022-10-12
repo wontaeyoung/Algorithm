94c93f48c41f02a091d5657d56133b543f7336e1
Taeyoung Won <hexter1994@gmail.com>
2022-10-12 21:29:37 +0900

    Create Lv.0 두 수의 합.swift

---
+++ Scripts/Programmers/Lv.0/Lv.0 두 수의 합.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0 두 수의 합.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/12.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 + num2
+}
