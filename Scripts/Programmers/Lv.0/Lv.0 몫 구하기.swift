1ed3caab1f5a7ede894e67b0c6c5229bc69c0f60
Taeyoung Won <hexter1994@gmail.com>
2022-10-13 21:40:18 +0900

    Create Lv.0 몫 구하기.swift

---
+++ Scripts/Programmers/Lv.0/Lv.0 몫 구하기.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0 몫 구하기.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/13.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 / num2
+}
