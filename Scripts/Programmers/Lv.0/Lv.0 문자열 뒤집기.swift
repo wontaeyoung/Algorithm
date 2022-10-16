7ca29460c936ce78fc2b4883a3f0b74643aec3f9
Taeyoung Won <hexter1994@gmail.com>
2022-10-16 23:24:42 +0900

    Create Lv.0 문자열 뒤집기.swift

---
+++ Scripts/Programmers/Lv.0/Lv.0 문자열 뒤집기.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0 문자열 뒤집기.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/16.
+//
+
+import Foundation
+
+func solution(_ my_string:String) -> String {
+    return String(my_string.reversed())
+}
