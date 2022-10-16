cfd6ca28982f9caa0df272975c32132c3f91b587
Taeyoung Won <hexter1994@gmail.com>
2022-10-16 23:56:04 +0900

    Create Lv.0 배열 두 배 만들기.swift

---
+++ Scripts/Programmers/Lv.0/Lv.0 배열 두 배 만들기.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0 배열 두 배 만들기.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/16.
+//
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    return numbers.map{$0 * 2}
+}
