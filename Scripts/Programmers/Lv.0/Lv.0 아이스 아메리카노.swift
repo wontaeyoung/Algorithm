392bfc5d20a3a9864e7904df7287369d8aef450c
Taeyoung Won <hexter1994@gmail.com>
2022-10-24 23:39:25 +0900

    Create Lv.0 아이스 아메리카노.swift

---
+++ Scripts/Programmers/Lv.0/Lv.0 아이스 아메리카노.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0 아이스 아메리카노.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/24.
+//
+
+import Foundation
+
+func solution(_ money:Int) -> [Int] {
+    return [money / 5500, money % 5500]
+}
