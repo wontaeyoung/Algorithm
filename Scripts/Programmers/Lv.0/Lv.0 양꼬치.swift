7029ca1a9dd6e56192b76f67e29a911343234ae4
Taeyoung Won <hexter1994@gmail.com>
2022-10-29 22:47:46 +0900

    Create Lv.0 양꼬치.swift

---
+++ Scripts/Programmers/Lv.0/Lv.0 양꼬치.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0 양꼬치.swift
+//  Algorithm
+//
+//  Created by 원태영 on 2022/10/29.
+//
+
+import Foundation
+
+func solution(_ n:Int, _ k:Int) -> Int {
+    return n * 12000 + k * 2000 - n / 10 * 2000
+}
